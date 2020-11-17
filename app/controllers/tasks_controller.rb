f5d7c6fdea38607cc9479ad3ab6363b7859138ad
Antal Bako <prowlersk8@gmail.com>
2020-11-17 18:56:53 +0100

    done to bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,19 +16,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
-  def destoy
-    @task = Task.find(params[:id])
-    @task.destoy
+  def destroy
+    @task.destroy
     redirect_to tasks_path
   end
 
@@ -37,5 +32,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :boolean)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
